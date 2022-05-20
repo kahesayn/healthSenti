033c50bce9e05cf4934688fd192d94c42d752a0c
Nati <kahesayn>
2022-05-20 11:39:38 -0400

    application update

--- healthSenti.py
+++ healthSenti.py
@@ -2,66 +2,119 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import tweepy as tw
-from twitterapi import *
+from twitter_data import *
 import altair as alt
 import wbgapi as wb
 import matplotlib.pyplot as plt
 from official_health import *
+from PIL import Image
+import datetime
 
 st.set_page_config(layout="wide")
-st.title("HealthSenti")
-st.header("Welcome to HealthSenti!")
 
-#get input and call twitter api
-topic = st.selectbox("Please choose a topic to research: ", ("Tuberculosis", "Malaria", "HIV/AIDS"))
+def main():
+    """main streamlit function"""
+    # load and display logo in the center
+    image = Image.open('data/logo.png')
+    col1, col2, col3 = st.columns([3,2,2])
+    with col1:
+        st.write("")
+    with col2:
+        st.image(image, width=200)
+    with col3:
+        st.write("")
 
-country = st.selectbox("Please choose a country: ", ("Ethiopia", "Nigeria", "India"))
 
-#choose between live or downloaded data
+    st.header("Welcome to HealthSenti!")
+    st.write("HealthSenti is an easy to use interactive application that is designed to visualize the public sentiment of a certain health topic and compare it to official health datasets, thus allowing for a clearer understanding of the state of health in the repective country or location. Please follow this link for more information.")
 
-start_time, end_time = st.select_slider("Please select a time range:",
-                    options=[2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022],
-                    value=(2006, 2022))
+    st.write("")
+
+    st.subheader("Application Mode:")
+    st.write("There are two ways to run this application, either the open data version or the closed data version. The open data version lets the user specify all the individual parameters, thus allowing for a more controlled and precise data visualization based on the user's specificities, which at the same time can be harder to operate. But this mode also requires that the user have their own Twitter API tokens with an Academic Research access level.")
+    st.write("The closed version on the other hand is the easier and more close ended version. It loads the data from already saved and processed data files. As such, there are limitations to what can be visualized, but it is much faster and simpler to run. Please choose one according to your specificities.")
+
+    mode = st.selectbox("Please choose the application mode: ", ("Closed Version", "Open Version"))
+    if mode == "Closed Version":
+        closed_ver()
+    if mode == "Open Version":
+        open_ver()
 
 
-official, public = st.columns(2)
+def closed_ver():
+    topic = st.selectbox("Please choose a topic to research: ", (None, "Tuberculosis", "Malaria", "HIV/AIDS"))
+    country = st.selectbox("Please choose a country: ", (None, "Ethiopia", "Nigeria", "India"))
 
-with official:
+    if topic and country:
+        start_time, end_time = st.select_slider("Please select a time range:",
+                            options=[2010, 2011, 2012, 2013, 2014, 2015],
+                            value=(2010, 2015))
+        official, public = st.columns(2)
+        with official:
+            official_option(topic, country, start_time, end_time)
+        with public:
+            public_option(topic, country, start_time, end_time, "closed")
+
+
+def open_ver():
+    topic = st.selectbox("Please choose a topic to research: ", (None, "Tuberculosis", "Malaria", "HIV/AIDS"))
+    country = st.selectbox("Please choose a country: ", (None, "Ethiopia", "Nigeria", "India", "Other"))
+    if country == "Other":
+        country = ("Please input the name of the country")
+    if topic and country:
+        current_year = datetime.datetime.today().year
+        years_list = list(range(current_year, 2005, -1))
+        years_list.reverse()
+        start_time, end_time = st.select_slider("Please select a time range:",
+                            options=years_list,
+                            value=(2006, int(current_year)))
+        official, public = st.columns(2)
+        with official:
+            official_option(topic, country, start_time, end_time)
+        with public:
+            df = None
+            data_type = st.selectbox("Please choose Twitter data source: ", (None, "Upload file", "Access Twitter API"))
+            if data_type == "Upload file":
+                df = upload()
+            elif data_type == "Access Twitter API":
+                df = live_twitter(topic, country, start_time, end_time)
+            public_option(topic, country, start_time, end_time, "open", df)
+
+
+def official_option(topic, country, start_time, end_time):
     st.subheader("Official HealthData:")
-    type_off = st.radio("Please choose between either downloaded or live twitter data. (For faster processing time, downloaded option is recommended.)", ("Downloaded", "Live"))
-    if type_off == "Downloaded":
-        downloaded_offdata(topic, country)
-    if type_off == "Live":
-        live_offdata(topic, country, start_time, end_time)
-with public:
+    type_source = st.radio("Please choose an offcial source: ", ("World Bank", "Gapminder"))
+    if type_source == "World Bank":
+        type_off = st.radio("Please choose between either downloaded or live twitter data. (For faster processing time, downloaded option is recommended.)", ("Downloaded", "Live"))
+        if type_off == "Downloaded":
+            downloaded_offdata(topic, country, start_time, end_time)
+        if type_off == "Live":
+            live_offdata(topic, country, start_time, end_time)
+    if type_source == "Gapminder":
+        gapminder(topic, country, start_time, end_time)
+
+
+def public_option(topic, country, start_time, end_time, type, df_open=None):
+    df = None
     st.subheader("Public Sentiment:")
-    type_sent = st.radio("Please choose a sentiment analyzer: ", ("Flair", "spaCy"))
-    type_pub = st.radio("Please choose between either downloaded or live official data. (For faster processing time, downloaded option is recommended.)", ("Downloaded", "Live"))
-    if type_pub == "Downloaded":
+    type_sent = st.radio("Please choose a sentiment analyzer: ", ("spaCy", "Flair"))
+    if type == "closed":
         df, df_dict = downloaded_twitter(topic, country, start_time, end_time)
+    if type == "open" and df_open is not None:
+        df, df_dict = open_twitter(topic, country, start_time, end_time, df_open)
+    if df is not None:
         df2 = analyzer(df, type_sent)
-        # st.write(df2)
         sent_dict = avg_sentiment(df_dict, type_sent)
-        # st.write(sent_dict)
-        graphsent(sent_dict)
-        # graph_sentiment(df2)
-    if type_pub == "Live":
-        live_twitter(topic, country)
+        graphsent(sent_dict, topic, country)
+
+        st.write("If you would like to view any other analysis, please select the one you wish to view from the below:")
+        check1 = st.checkbox("Wordcloud of top 50 prevelant words")
+        if check1:
+            word_cloud(df, country, topic)
+        check2 = st.checkbox("Top 10 words by frequency")
+        if check2:
+            check_freq(df)
 
 
-#store and show aggregated tweets
-
-
-
-# simple polarity chart for tweets
-# c = alt.Chart(tweet_text).mark_circle().encode(x='time', y='sentiment')
-
-# call the world bank api
-
-
-# col1, col2 = st.beta_columns(2)
-# with col1:
-#     st.header("Twitter Sentiment Analysis")
-#     st.altair_chart(c, use_container_width=True)
-# with col2:
-#     st.header("World Bank Data Analysis")
+if __name__ == "__main__":
+    main()
--- official_health.py
+++ official_health.py
@@ -3,8 +3,11 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import altair as alt
+import pycountry
+from twitter_data import get_code
 
-def downloaded_offdata(topic, country):
+def downloaded_offdata(topic, country, start_time, end_time):
+    df = None
     if country == "Ethiopia":
         df = pd.read_csv (r'data/Official/EthiopiaData.csv')
     if country == "Nigeria":
@@ -12,50 +15,82 @@ def downloaded_offdata(topic, country):
     if country == "India":
         df = pd.read_csv (r'data/Official/IndiaData.csv')
 
-    if (topic == "Malaria"):
-        c = alt.Chart(df, title="Malaria Incidence").mark_line().encode(x='Year', y='Malaria-Incidence')
-        st.altair_chart(c, use_container_width=True)
-    if(topic == "Tuberculosis"):
-        c = alt.Chart(df, title="Tuberculosis Incidence").mark_line().encode(x='Year', y='TB-Incidence')
-        st.altair_chart(c, use_container_width=True)
-    if (topic == "HIV/AIDS"):
-        c = alt.Chart(df, title="HIV/AIDS Incidence").mark_line().encode(x='Year', y='HIV-Incidence')
-        st.altair_chart(c, use_container_width=True)
+    if not df.empty:
+        start_time -= 2001
+        end_time -= 2000
+        df = df.iloc[start_time:end_time]
+        ccode, tcode = get_code(country, topic)
+        title = topic + " Incidence in " + country
+        col = "" + tcode + "-Incidence"
+
+        fig, ax = plt.subplots()
+        x = df['Year']
+        y = df[col]
+        ax.plot(x, y)
+        ax.set_title(title)
+        ax.set_xlabel('Time')
+        ax.set_ylabel(topic + ' Incidence')
+        st.pyplot(ax.figure)
+
+def gapminder(topic, country, start_time, end_time):
+    df = pd.read_csv (r'data/Official/GapminderData.csv')
+    df2 = df[df.Country == country]
+    df2 = df2.set_index('Year')
+    df2 = df2.loc[str(start_time):str(end_time)]
+    ccode, tcode = get_code(country, topic)
+    title = topic + " Incidence in " + country
+    col = "" + tcode + "-Incidence"
+
+    fig1, ax1 = plt.subplots()
+    x = df2.index
+    y = df2[col]
+    ax1.plot(x, y)
+    ax1.set_title(title)
+    ax1.set_xlabel('Time')
+    ax1.set_ylabel(topic + ' Incidence')
+    st.pyplot(ax1.figure)
 
 def live_offdata(topic, country, start_time, end_time):
     getWBdata(topic, country, start_time, end_time)
 
 def getWBdata(topic, country, start_time, end_time):
     end_time +=1
-    if (country == "Ethiopia"):
-        ccode = 'ETH'
-    if (country == "India"):
-        ccode = 'IND'
-    if (country == "Nigeria"):
-        ccode = 'NGA'
-
+    mapping = {country.name: country.alpha_3 for country in pycountry.countries}
+    ccode = mapping.get(country)
     if (topic == "Malaria"):
-        res = malaria(ccode, start_time, end_time)
+        res = malaria(ccode, start_time, end_time, country, topic)
     if(topic == "Tuberculosis"):
-        res = tb(ccode, start_time, end_time)
+        res = tb(ccode, start_time, end_time, country, topic)
     if (topic == "HIV/AIDS"):
-        res = hiv(ccode, start_time, end_time)
+        res = hiv(ccode, start_time, end_time, country, topic)
 
 
-def tb(ccode, start_time, end_time):
-    n = wb.data.DataFrame(['SH.TBS.INCD'], ccode, range(start_time, end_time))
-    n1 = n.transpose()
-    n1 = n1.apply(pd.to_numeric)
-    st.line_chart(n1[ccode])
+def tb(ccode, start_time, end_time, country, topic):
+    df = wb.data.DataFrame(['SH.TBS.INCD'], ccode, range(start_time, end_time))
+    df1 = df.transpose()
+    df1 = df1.apply(pd.to_numeric)
+    graph_official(topic, country, df1, ccode)
 
-def malaria(ccode, start_time, end_time):
-    n = wb.data.DataFrame(['SH.MLR.INCD.P3'], ccode, range(start_time, end_time))
-    n1 = n.transpose()
-    n1 = n1.apply(pd.to_numeric)
-    st.line_chart(n1[ccode])
+def malaria(ccode, start_time, end_time, country, topic):
+    df = wb.data.DataFrame(['SH.MLR.INCD.P3'], ccode, range(start_time, end_time))
+    df1 = df.transpose()
+    df1 = df1.apply(pd.to_numeric)
+    graph_official(topic, country, df1, ccode)
 
-def hiv(ccode, start_time, end_time):
-    n = wb.data.DataFrame(['SH.HIV.INCD.TL.P3'], ccode, range(start_time, end_time))
-    n1 = n.transpose()
-    n1 = n1.apply(pd.to_numeric)
-    st.line_chart(n1[ccode])
+def hiv(ccode, start_time, end_time, country, topic):
+    df = wb.data.DataFrame(['SH.HIV.INCD.TL.P3'], ccode, range(start_time, end_time))
+    df1 = df.transpose()
+    df1 = df1.apply(pd.to_numeric)
+    graph_official(topic, country, df1, ccode)
+
+def graph_official(topic, country, df1, ccode):
+    df1.reset_index(inplace=True)
+    fig, ax = plt.subplots()
+    title = topic + " Incidence in " + country
+    x = df1['index']
+    y = df1[ccode]
+    ax.plot(x,y)
+    ax.set_title(title)
+    ax.set_xlabel('Time')
+    ax.set_ylabel(topic + ' Incidence')
+    st.pyplot(ax.figure)
